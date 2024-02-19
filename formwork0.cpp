c4ec8fb27772b6a484f8041bb7df86f256fefa0a
coper <76933810+coperlm@users.noreply.github.com>
2024-02-20 05:35:27 +0800

    description

---
+++ formwork0.cpp
@@ -0,0 +1,30 @@
+#include<bits\stdc++.h>
+#define int long long
+
+using namespace std;
+
+const int N = 2e5;
+const int mod = 1e9+7;
+int n , a[N+100];
+
+
+void solve()
+{
+    cin >> n;
+    for(int i = 1;i <= n;i ++)
+    {
+        cin >> a[i];
+        
+    }
+}
+
+signed main()
+{
+    int t = 1;
+    cin >> t;
+    while( t -- )
+    {
+        solve();
+    }
+    return 0;
+}
